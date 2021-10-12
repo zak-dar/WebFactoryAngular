28f695dfec5e685e2722636f19daa2a692da787b
Jessica <jessybahout.jb@gmail.com>
2021-10-12 16:08:04 +0300

    test3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CursussComponent } from './cursuss/cursuss.component';
 import { MatieresComponent } from './matieres/matieres.component';
 
 const routes: Routes = [
-  {path: "matieres", component: MatieresComponent}
+  {path: "matieres", component: MatieresComponent},
+  {path: "cursus", component: CursussComponent}
 ];
 
 @NgModule({
