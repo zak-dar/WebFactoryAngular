5507a0a59e0e7bf7b2e135195899dab6ed06f0d7
Zakaria <zakaria.darrah@gmail.com>
2021-10-12 16:19:16 +0200

    formateur zak

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CursussComponent } from './cursuss/cursuss.component';
+import { FormateursComponent } from './formateurs/formateurs.component';
 import { MatieresComponent } from './matieres/matieres.component';
 import { StagiaireComponent } from './stagiaire/stagiaire.component';
 
 const routes: Routes = [
   {path: "matieres", component: MatieresComponent},
   {path: "cursus", component: CursussComponent},
-  {path: "stagiaire", component: StagiaireComponent}
+  {path: "stagiaire", component: StagiaireComponent},
+  {path : "formateurs", component : FormateursComponent}
 
 ];
 
