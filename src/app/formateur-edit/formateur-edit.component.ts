efc6c64b99d28ec6c8a7d5cb718164621fbcf7cc
Zakaria <zakaria.darrah@gmail.com>
2021-10-12 16:39:06 +0200

    formateur fini

--- src/app/formateur-edit/formateur-edit.component.ts
+++ src/app/formateur-edit/formateur-edit.component.ts
@@ -14,15 +14,13 @@ export class FormateurEditComponent {
   listCompetences: Array<Matiere>;
 
 
-
   constructor(private formateurService: FormateurService, private matiereService: MatiereService) {
   }
 
   ngOnChanges(): void {
-    console.log(this.formateur)
     if (this.formateur) {
       this.listCompetences = this.matiereService.findAll();
-      
+
       for (let c of this.listCompetences) {
         c.Checked = (this.formateur.Competences.find(m => m.Id == c.Id) != null)
       }
@@ -32,30 +30,23 @@ export class FormateurEditComponent {
   }
 
   save(): void {
-    console.log(this.listCompetences);
-    // if (this.formateur.Id) {
-    //   this.formateurService.update(this.formateur);
-    // } else {
-    //   this.formateurService.create(this.formateur);
-    // }
-    // this.cancel();
+    this.formateur.Competences = new Array<Matiere>();
+    for (let competence of this.listCompetences) {
+      if (competence.Checked == true) {
+        this.formateur.Competences.push(competence)
+      }
+    }
+    if (this.formateur.Id) {
+      this.formateurService.update(this.formateur);
+    } else {
+
+      this.formateurService.create(this.formateur);
+    }
+    this.cancel();
   }
 
   cancel(): void {
     this.formateur = null;
   }
-
-  // getCompetences(): Array<Matiere> {
-  //   if (this.formateur) {
-  //     for(let comp of this.formateur.Competences){
-  //       let i = 0;
-  //       if(this.matiereService.find(i).Id )
-  //     }
-  //     return this.formateur.Competences
-  //   } else {
-  //     return this.matiereService.findAll()
-  //   }
-  // }
-
 
 }
